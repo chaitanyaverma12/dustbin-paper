ac96892979700d6bec39b3d9a985db657af30e1d
chaitanyaverma12 <74818653+chaitanyaverma12@users.noreply.github.com>
2021-01-08 19:23:33 +0530

    use dustbin

---
+++ Paper.js
@@ -0,0 +1,23 @@
+class Paper{
+    constructor(){
+        var options ={
+            
+            restitution :0,
+            friction :0,
+            density :1.2
+            
+        }
+        this.body = Bodies.circle(250,540,options);
+        this.width =33;
+        this.image = loadImage("paper (1).png");
+        World.add(world, this.body);
+    }//250,540,20,option
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        fill("white");
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,33,33);
+    }
+} 
